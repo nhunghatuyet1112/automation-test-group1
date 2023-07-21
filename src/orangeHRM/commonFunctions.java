f362ea45cf9e7f4d73421009679d7fc6ef2acb61
Huy0811 <duongchihuy.study@gmail.com>
2023-07-21 19:26:02 +0700

    AutomationTest-OrangeHRM

--- src/orangeHRM/commonFunctions.java
+++ src/orangeHRM/commonFunctions.java
@@ -1,5 +1,6 @@
 package orangeHRM;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -9,7 +10,7 @@ import org.openqa.selenium.WebElement;
 public class commonFunctions {
 	public static void toLogin(WebDriver driver, String baseUrl) throws Exception {
 		driver.get(baseUrl);
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		WebElement userNameInput = driver.findElement(By.xpath("//input[@name='username']"));
 		userNameInput.sendKeys("Admin");
 		WebElement passwordInput = driver.findElement(By.xpath("//input[@name='password']"));
@@ -20,7 +21,7 @@ public class commonFunctions {
 	}
 
 	public static void toSelectModule(WebDriver driver, String moduleName) throws Exception {
-		Thread.sleep(5000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		List<WebElement> modules = driver.findElements(By.xpath("//li[@class=\"oxd-main-menu-item-wrapper\"]"));
 		for (int i = 0; i < modules.size(); i++) {
 			if (modules.get(i).getText().equals(moduleName)) {
@@ -32,7 +33,7 @@ public class commonFunctions {
 
 	public static void toSelectSection(WebDriver driver, String sectionName, boolean isSelectChild,
 			String childSectionName) throws Exception {
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		List<WebElement> sections = driver.findElements(By.xpath("//li[@class='oxd-topbar-body-nav-tab --parent']"));
 		for (int i = 0; i < sections.size(); i++) {
 			if (sections.get(i).getText().equals(sectionName)) {
@@ -40,7 +41,7 @@ public class commonFunctions {
 				break;
 			}
 		}
-		Thread.sleep(1000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
 		if (isSelectChild == true) {
 			List<WebElement> selectedSection = driver
 					.findElements(By.xpath("//li[@class='--active oxd-topbar-body-nav-tab --parent']/ul/li"));
@@ -56,7 +57,7 @@ public class commonFunctions {
 	}
 
 	public static void selectToEdit(WebDriver driver, String selectedName) throws Exception {
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		List<WebElement> nameColumns = driver
 				.findElements(By.xpath("//div[@class='oxd-table-body']/div[@class='oxd-table-card']/div/div[2]"));
 		for (int i = 0; i < nameColumns.size(); i++) {
@@ -72,10 +73,10 @@ public class commonFunctions {
 	}
 
 	public static void toAddDemoCustomer(WebDriver driver, String customerName, String customerDescription) throws Exception {
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		WebElement addButton = driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']"));
 		addButton.click();
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		WebElement name = driver.findElement(By.xpath("//form//div[@class='oxd-form-row'][1]//input"));
 		name.sendKeys(customerName);
 		WebElement description = driver.findElement(By.xpath("//form//div[@class='oxd-form-row'][2]//textarea"));
--- src/orangeHRM/deleteCustomer.java
+++ src/orangeHRM/deleteCustomer.java
@@ -1,5 +1,6 @@
 package orangeHRM;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
