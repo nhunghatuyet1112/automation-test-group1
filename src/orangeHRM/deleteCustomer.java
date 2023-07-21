d4990ef32dc70dfab9fe4ea93c3a03da70e0e698
Huy0811 <duongchihuy.study@gmail.com>
2023-07-21 14:30:53 +0700

    AutomationTest:OrangeHRM

--- src/orangeHRM/deleteCustomer.java
+++ src/orangeHRM/deleteCustomer.java
@@ -1,5 +1,7 @@
 package orangeHRM;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -26,7 +28,7 @@ public class deleteCustomer extends commonFunctions {
 		commonFunctions.toLogin(driver, baseUrl);
 	}
 
-	@Test(enabled = false)
+	@Test
 	public void TC_OHRM_DC_01() throws Exception {
 		String expectedResult = "Successfully Deleted";
 		String customerName = "Van Lang University - Group 1";
@@ -51,7 +53,7 @@ public class deleteCustomer extends commonFunctions {
 		Assert.assertEquals(actualResult, expectedResult);
 	}
 
-	@Test(enabled = false)
+	@Test
 	public void TC_OHRM_DC_02() throws Exception {
 		String expectedResult = "Successfully Deleted";
 		String customerName = "Van Lang University - Group 1";
@@ -83,7 +85,7 @@ public class deleteCustomer extends commonFunctions {
 		Assert.assertEquals(actualResult, expectedResult);
 	}
 
-	@Test(enabled = false)
+	@Test
 	public void TC_OHRM_DC_03() throws Exception {
 		String expectedResult = "Successfully Deleted";
 		String customerName1 = "Van Lang University - Group 1 - 01";
@@ -118,8 +120,8 @@ public class deleteCustomer extends commonFunctions {
 		jse.executeScript("window.scrollBy(0,200)");
 		Assert.assertEquals(actualResult, expectedResult);
 	}
-	
-	@Test(enabled = false)
+
+	@Test
 	public void TC_OHRM_DC_04() throws Exception {
 		String expectedResult = "Successfully Deleted";
 		String customerName1 = "Van Lang University - Group 1 - 01";
@@ -138,8 +140,8 @@ public class deleteCustomer extends commonFunctions {
 		Thread.sleep(1000);
 		jse.executeScript("window.scrollBy(0,200)");
 		Thread.sleep(1000);
-		WebElement trashIcon = driver.findElement(By.xpath("//div[@class='oxd-table-body']//div[text()='" + customerName1
-				+ "']/parent::div/following-sibling::div[2]//i[@class='oxd-icon bi-trash']"));
+		WebElement trashIcon = driver.findElement(By.xpath("//div[@class='oxd-table-body']//div[text()='"
+				+ customerName1 + "']/parent::div/following-sibling::div[2]//i[@class='oxd-icon bi-trash']"));
 		trashIcon.click();
 		Thread.sleep(1000);
 		WebElement deleteButton = driver.findElement(By.xpath(
@@ -151,11 +153,11 @@ public class deleteCustomer extends commonFunctions {
 		String actualResult = toastResult.getText();
 		Assert.assertEquals(actualResult, expectedResult);
 	}
-	
+
 	@Test
 	public void TC_OHRM_DC_05() throws Exception {
 		String expectedResult = "Not allowed to delete customer(s) associated with projects that have time logged against them";
-		String customerName = "ACME Ltd";
+		String customerName = "The Coca-Cola Company";
 		commonFunctions.toSelectModule(driver, "Time");
 		commonFunctions.toSelectSection(driver, "Project Info", true, "Customers");
 		Thread.sleep(5000);
@@ -168,6 +170,60 @@ public class deleteCustomer extends commonFunctions {
 		WebElement toastResult = driver.findElement(
 				By.xpath("//p[@class='oxd-text oxd-text--p oxd-text--toast-message oxd-toast-content-text']"));
 		String actualResult = toastResult.getText();
+		Assert.assertEquals(actualResult, expectedResult);
+	}
+
+	@Test
+	public void TC_OHRM_DC_06() throws Exception {
+		Boolean expectedResult = true;
+		Boolean actualResult = false;
+		String customerName = "Van Lang University - Group 1 - 03";
+		String customerDescription = "Automation Test";
+		commonFunctions.toSelectModule(driver, "Time");
+		commonFunctions.toSelectSection(driver, "Project Info", true, "Customers");
+		commonFunctions.toAddDemoCustomer(driver, customerName, customerDescription);
+		Thread.sleep(10000);
+		jse.executeScript("window.scrollBy(0,200)");
+		Thread.sleep(1000);
+		WebElement trashIcon = driver.findElement(By.xpath("//div[@class='oxd-table-body']//div[text()='" + customerName
+				+ "']/parent::div/following-sibling::div[2]//i[@class='oxd-icon bi-trash']"));
+		trashIcon.click();
+		Thread.sleep(1000);
+		WebElement cancelButton = driver.findElement(
+				By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--text orangehrm-button-margin']"));
+		cancelButton.click();
+		List<WebElement> customerNameList = driver
+				.findElements(By.xpath("//div[@class='oxd-table-body']//div[text()='" + customerName + "']"));
+		if (customerNameList.size() > 0) {
+			actualResult = true;
+		}
+		Assert.assertEquals(actualResult, expectedResult);
+	}
+
+	@Test
+	public void TC_OHRM_DC_07() throws Exception {
+		Boolean expectedResult = true;
+		Boolean actualResult = false;
+		String customerName = "Van Lang University - Group 1 - 04";
+		String customerDescription = "Automation Test";
+		commonFunctions.toSelectModule(driver, "Time");
+		commonFunctions.toSelectSection(driver, "Project Info", true, "Customers");
+		commonFunctions.toAddDemoCustomer(driver, customerName, customerDescription);
+		Thread.sleep(10000);
+		jse.executeScript("window.scrollBy(0,200)");
+		Thread.sleep(1000);
+		WebElement trashIcon = driver.findElement(By.xpath("//div[@class='oxd-table-body']//div[text()='" + customerName
+				+ "']/parent::div/following-sibling::div[2]//i[@class='oxd-icon bi-trash']"));
+		trashIcon.click();
+		Thread.sleep(1000);
+		WebElement xButton = driver
+				.findElement(By.xpath("//button[@class='oxd-dialog-close-button oxd-dialog-close-button-position']"));
+		xButton.click();
+		List<WebElement> customerNameList = driver
+				.findElements(By.xpath("//div[@class='oxd-table-body']//div[text()='" + customerName + "']"));
+		if (customerNameList.size() > 0) {
+			actualResult = true;
+		}
 		Assert.assertEquals(actualResult, expectedResult);
 	}
 }
