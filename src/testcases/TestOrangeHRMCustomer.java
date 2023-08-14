776d08034b2e13f2a240c69ab131caadc7cacbd4
Nhung Ha <nhunghatuyet111202@gmail.com>
2023-08-14 14:53:27 +0700

    Import Reporter Class

--- src/testcases/TestOrangeHRMCustomer.java
+++ src/testcases/TestOrangeHRMCustomer.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
